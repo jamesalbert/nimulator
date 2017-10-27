ef67c8632dc3384e3723a12d04ff49882a0fedeb
jamesalbert <jalbert@cloudbees.com>
2017-10-27 00:43:01 -0700

    checkpoint - started emulator

--- nimulator.nim
+++ nimulator.nim
@@ -6,6 +6,8 @@ import tables
 import os
 
 
+# Assembly
+
 let maxPadding = {
   "mem": 13,
   "cmd": 3,
@@ -76,14 +78,14 @@ method bytes(self: Translator) {.base.} =
 method hexdump(self: Translator) {.base.} =
   self.bytes()
   self.hexSeq = map[uint8, string](self.binSeq, proc(x: uint8): string =
-    toHex(x).toLower)
+    toHex(x).toLowerAscii)
 
 method parseInstruction(self: Translator) {.base.} =
   var i: seq[string] = splitWhitespace(self.instruction)
   var p: seq[string] = @[]
   if i.len > 1:
-    p = map(split(i.pop(), sep=','), toLower)
-  self.command = i.pop().toLower()
+    p = map(split(i.pop(), sep=','), toLowerAscii)
+  self.command = i.pop().toLowerAscii
   self.params = p
 
 method nextop(self: Translator) {.base.} =
@@ -111,8 +113,8 @@ method stretch(self: Translator, start: int) {.base.} =
 
 method prettify(self: Translator) {.base.} =
   var mem: string = self.adr & " " & self.hex & " " & self.hexParams
-  var params = join(self.params, sep=",").toLower()
-  var comment = "(" & $self.origSize & ") " & self.function.toLower()
+  var params = join(self.params, sep=",").toLowerAscii
+  var comment = "(" & $self.origSize & ") " & self.function.toLowerAscii
   write(outfile, padding(mem, "mem") &
                  padding(self.command, "cmd") &
                  padding(params, "arg") &
@@ -133,7 +135,7 @@ method translate(self: Translator) {.base.} =
     for i_adr in 1..self.size:
       self.hexParams &= self.hexSeq[i + i_adr] & " "
     self.stretch(i)
-    self.adr = toHex(i, 4).toLower()
+    self.adr = toHex(i, 4).toLowerAscii
     self.prettify()
     inc(i, self.origSize)
 
@@ -145,5 +147,108 @@ proc newTranslator(filename: string): Translator =
   return t
 
 
-var t: Translator = newTranslator("assets/mario.nes")
-t.translate()
+# Emulation
+
+type ConditionCodes = ref object
+  z: uint8
+  s: uint8
+  p: uint8
+  cy: uint8
+  ac: uint8
+  pad: uint8
+
+proc newConditionCodes(): ConditionCodes =
+  return ConditionCodes(z: 1, s: 1, p: 1, cy: 1, ac: 1, pad: 3)
+
+
+type State8080 = ref object
+  a: uint8
+  b: uint8
+  c: uint8
+  d: uint8
+  e: uint8
+  h: uint8
+  l: uint8
+  sp: uint16
+  pc: int
+  memory: ref uint8
+  cc: ConditionCodes
+  int_enable: uint8
+  binSeq: seq[uint8]
+
+method unimplemented(self: State8080, opcode: uint8) {.base.} =
+  return
+  # echo "error: unimplemented instruction '", opcode, "'"
+  # quit(1)
+
+method noop(self: State8080) {.base.} =
+  return
+
+method accumulate(self: State8080, register: uint8): uint16 {.base.} =
+  return (uint16)(self.a) + (uint16)(register)
+
+
+method evalFlags(self: State8080, acc: uint16) {.base.} =
+  self.cc.z = (uint8)((acc and 0xff) == (uint16)0)
+  self.cc.s = (uint8)((acc and 0x80) != (uint16)0)
+  self.cc.cy = (uint8)(acc > (uint16)0xff)
+  # self.cc.p = parity(acc and 0xff)
+  self.a = acc and 0xff
+
+method emulate(self: State8080) {.base.} =
+  var opcode: uint8 = self.binSeq[self.pc]
+  case opcode
+  of 0x00: self.noop()
+  of 0x01: # LXI B,D16
+    self.c = self.binSeq[self.pc+1]
+    self.b = self.binSeq[self.pc+2]
+    self.pc += 2
+  of 0x41: # MOV B,C
+    self.b = self.c
+  of 0x42: # MOV B,D
+    self.b = self.d
+  of 0x43: # MOV B,E
+    self.b = self.e
+  of 0x80: # ADD B
+    var acc = self.accumulate(self.b)
+    self.evalFlags(acc)
+  of 0x81: # ADD C
+    var acc = self.accumulate(self.c)
+    self.evalFlags(acc)
+  of 0xc6: # ADI byte
+    var acc = self.accumulate(self.binSeq[self.pc+1])
+    self.evalFlags(acc)
+  of 0x86: # ADD M
+    var offset: uint16 = (self.h shl 8) or self.l
+    var acc: uint16 = (uint16) self.a + self.binSeq[(int)offset]
+    self.evalFlags(acc)
+  of 0xc2: # JNZ address
+    if self.cc.z == 0:
+      self.pc = (int)((self.binSeq[self.pc+2] shl 8) or self.binSeq[self.pc+1])
+    else:
+      self.pc += 2
+  of 0xc3: # JMP address
+    self.pc = (int)((self.binSeq[self.pc+2] shl 8) or self.binSeq[self.pc+1])
+  of 0xcd: # CALL address
+    var ret: uint16 = (uint16)(self.pc + 2)
+    self.binSeq[self.pc-1] = (ret shr 8) and 0xff
+    self.binSeq[self.pc-2] = ret and 0xff
+    self.sp = self.sp - 2
+    self.pc = (int)((self.binSeq[self.pc+2] shl 8) or self.binSeq[self.pc+1])
+  of 0xc9: # RET
+    self.pc = (int)(self.binSeq[(int)self.sp] or (self.binSeq[(int)self.sp+1] shl 8))
+    self.sp += 2
+  else:
+    self.unimplemented(opcode)
+  self.pc += 1
+
+proc newState8080(): State8080 =
+  var t: Translator = newTranslator("assets/mario.nes")
+  var s: State8080
+  t.translate()
+  s = State8080(binSeq: t.binSeq, cc: newConditionCodes())
+  while true:
+    s.emulate()
+  return s
+
+discard newState8080()
